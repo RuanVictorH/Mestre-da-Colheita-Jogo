30e06b34f4f1c980d5a10706a41abce0d7c3978b
RuanVictorH <132318191+RuanVictorH@users.noreply.github.com>
2024-12-07 15:26:41 -0300

    Add files via upload

---
+++ JOGO-IPOO/JOGO-IPOO/Jogo-IPOO/MyWorld.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Classe Menu.
+ * 
+ * @author (Alex, Pedro e Ruan) 
+ * @version (07.12.2024)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        super(1100, 600, 1); // Configura o mundo com dimensões 1500x700
+        GreenfootImage imagemMenu = new GreenfootImage("Imagem-Menu.png"); // Carrega a imagem
+        imagemMenu.scale(getWidth(), getHeight()); // Redimensiona para caber no mundo
+        setBackground(imagemMenu); // Define a imagem como fundo
+    }
+}
